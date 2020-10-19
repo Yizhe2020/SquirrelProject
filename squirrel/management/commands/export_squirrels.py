3802a39f32df4f44be73f071043e58a00eff7fed
Jiaqi Zhu <jz3282@columbia.edu>
2020-10-19 18:05:43 +0000

    Write the export_squrrels.py

---
+++ squirrel/management/commands/export_squirrels.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+
+import csv
+
+from django.core.management.base import BaseCommand
+
+from squirrel.models import Data
+
+
+class Command(BaseCommand):
+    help = 'export the squirrel data csv'
+
+    def add_arguments(self,parser):
+        parser.add_argument('csv_file', help = 'file exported')
+
+    def handle(self, *args, **options):
+        file_= options['csv_file']
+
+        with open(file_, 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Latitude','Longitude','Unique Squirrel ID','Shift','Date','Age',
+                'Primary Fur Color','Location','Specific Location','Running','Chasing','Climbing','Eating','Foraging',
+                'Other Activity','Kuks','Quaas','Moans','Tail Flags','Tail Twitches','Approaches',
+                'Indifferent','Runs_From'])
+            for obj in Data.objects.all().values_list('Latitude','Longitude','Unique_Squirrel_ID','Shift','Date','Age',
+                'Primary_Fur_Color','Location','Specific_Location','Running','Chasing','Climbing','Eating','Foraging',
+                'Other_Activity','Kuks','Quaas','Moans','Tail_Flags','Tail_Twitches','Approaches',
+                'Indifferent','Runs_From'):
+                writer.writerow(obj)
+
+            csvfile.close()
+
+        msg = f'you are exporting from {file_}'
+        self.stdout.write(self.style.SUCCESS(msg))
