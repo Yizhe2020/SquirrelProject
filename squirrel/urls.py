c4b6b8461d8ee5d9ba50207a1a9f9551279dfada
Jiaqi Zhu <jz3282@columbia.edu>
2020-10-20 04:46:57 +0000

    Modified views.py and urls.py for sightings/stats, sightings/<Unique_Squirrel_ID> and sightings/add

--- squirrel/urls.py
+++ squirrel/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'squirrel'
 
 urlpatterns = [
     path('', views.home_page, name = 'index'),
     path('map/', views.map, name = 'map'),
     path('sightings/', views.sightings, name = 'sightings'),
+    path('sightings/<Unique_Squirrel_ID>/', views.update_sightings, name = 'update_sightings'),
+    path('sightings/add/', views.create_sightings, name = 'create_sightings'),
+    path('sightings/stats/', views.stats, name = 'stats'),
 ]
--- squirrel/views.py
+++ squirrel/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Data
-
+from .form
 def home_page(request):
     squirrels = 'Squirrel Tracker'
     return render(request,'squirrel/index.html',{'Squirrels': squirrels})
@@ -13,4 +13,37 @@ def map(request):
 def sightings(request):
     squirrels = Data.objects.all()
     return render(request,'squirrel/sightingslist.html',{'Squirrel': squirrels})
+
+def update_sightings(request, Unique_Squirrel_ID):
+    squirrels = Data.objects.get(Unique_Squirrel_ID = Unique_Squirrel_ID)
+    if request.method == 'POST':
+        form = UpdateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/sightings/')
+    else:
+        form = UpdateForm(instance = squirrels)
+    return render(request,'squirrel/update_sightings.html', {'Form': form})
+
+def create_sightings(request):
+    if request.method == 'POST':
+        form = CreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/sightings/')
+    else:
+        form = CreateForm()
+    return render(request,'squirrel/create_sightings.html',{'Form': form})
+
+def stats(request):
+    squirrels = Data.objects.all()
+    PM_Count = squirrels.filter(Shift = 'PM').count()
+    Cinnamon_Count = squirrels.filter(Primary_Fur_Color = 'Cinnamon').count()
+    Eating_Count = squirrels.filter(Eating = 'True').count()
+    Moans_Count = squirrels.filter(Moans = 'True').count()
+    Indifferent_Count = squirrels.filter(Indifferent = 'True').count()
+    return render(request,'squirrel/stats.html', {'PM_Count': PM_Count, 'Cinnamon_Count': Cinnamon_Count, 'Eating_Count': Eating_Count,
+        'Moans_Count': Moans_Count, 'Indifferent_Count': Indifferent_Count})
+
+
 # Create your views here.
